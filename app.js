0ebb99129a7e9b0a489a4f437d3c0b031787abf3
Chris Martin <cj@cjmart.in>
2014-05-29 21:02:21 -0700

    start serial stuff

--- app.js
+++ app.js
@@ -4,11 +4,40 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var serialport = require("serialport").SerialPort;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+/**
+ * Serial Port Setup.
+ */
+ 
+var portName = '/dev/ttyAMA0'; //This is the standard Raspberry Pi Serial port
+ 
+var readData = ''; //Array to hold the values read in from the port
+ 
+var sp = new serialport(portName, {
+	baudRate: 9600,
+	dataBits: 8,
+	parity: 'none',
+	stopBits: 1,
+	flowControl: false
+});
+
+sp.on('close', function (err) {
+	console.log('port closed');
+});
+
+sp.on('error', function (err) {
+	console.error("error", err);
+});
+
+sp.on('open', function () {
+	console.log('port opened...');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,9 +55,9 @@ app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 /// error handlers
@@ -36,23 +65,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
